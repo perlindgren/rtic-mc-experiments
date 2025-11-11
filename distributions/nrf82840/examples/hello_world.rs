c9ad096bf9ebed3d31f96c434a1b806b4a2145e1
Per Lindgren <per.lindgren@ltu.se>
2025-11-11 23:47:50 +0100

    v7m wip

---
+++ distributions/nrf82840/examples/hello_world.rs
@@ -0,0 +1,87 @@
+//! examples/hello_world.rs
+
+#![deny(unsafe_code)]
+// #![deny(warnings)]
+#![no_main]
+#![no_std]
+
+// #[rtic::app(device=rp2040_hal::pac, peripherals=false, dispatchers=[DMA_IRQ_0])]
+
+#[rtic::app(device = nrf52840_hal::pac, dispatchers= [])]
+mod app {
+    use cortex_m::asm;
+    //, peripheral::NVIC};
+    use defmt::*;
+    use defmt_rtt as _;
+    use nrf52840_hal::{self as _, pac};
+    use panic_probe as _;
+
+    #[shared]
+    struct SharedResources {
+        x: u64,
+    }
+
+    #[init]
+    fn system_init() -> SharedResources {
+        for i in 0..10 {
+            println!("hello_world #{}", i);
+        }
+
+        SharedResources { x: 0 }
+    }
+
+    #[idle(shared = [x])]
+    pub struct MyIdleTask {
+        count: u32,
+    }
+    impl RticIdleTask for MyIdleTask {
+        fn init() -> Self {
+            Self { count: 0 }
+        }
+
+        fn exec(&mut self) -> ! {
+            self.shared().x.lock(|_| {});
+            loop {
+                self.count += 1;
+                println!("looping in idle... {}", self.count);
+                rtic::export::NVIC::pend(pac::Interrupt::TIMER0);
+                asm::delay(48_000_000);
+            }
+        }
+    }
+
+    #[task(priority = 7, binds = TIMER0, shared = [x])]
+    pub struct T0 {
+        count: u32,
+    }
+    impl RticTask for T0 {
+        fn init() -> Self {
+            Self { count: 0 }
+        }
+
+        fn exec(&mut self) {
+            self.shared().x.lock(|_| {
+                self.count += 1;
+                println!("Timer0... {}", self.count);
+                rtic::export::NVIC::pend(pac::Interrupt::TIMER1);
+                println!("Timer0... {}", self.count);
+            });
+        }
+    }
+
+    #[task(priority = 8, binds = TIMER1, shared = [x])]
+    pub struct T2 {
+        count: u32,
+    }
+    impl RticTask for T2 {
+        fn init() -> Self {
+            Self { count: 0 }
+        }
+
+        fn exec(&mut self) {
+            self.shared().x.lock(|_| {});
+            self.count += 2;
+            println!("Timer1... {}", self.count);
+        }
+    }
+}
